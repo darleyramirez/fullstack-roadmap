b1146785f2425e682c0cece68d54cff71047bdbb
darleyramirez <daraxen4@gmail.com>
2025-11-27 14:54:14 -0500

    Add diferencia function to calculate difference from 13
    Implement a function to calculate the difference from 13, returning double if greater.

---
+++ Día 14/Task/0-ejercicio.js
@@ -0,0 +1,15 @@
+// Escribe un programa en javaScript para obtener la diferencia entre un valor dado y 13, si el número es 
+// Mayor a 13  devuelve el doble de la diferencia absoluta
+
+function diferencia(valorDado){
+    if(valorDado <= 13){
+     let total = valorDado - 13;   
+     return total;
+    }else{
+         valorDado > 13
+        let total = valorDado - 13;
+         return total * 2;
+    }
+}
+console.log(diferencia(20));
+console.log(diferencia(13));
