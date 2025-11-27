8ba92bf4d069c70ec8a38f528f0a34d24bcbdfbf
darleyramirez <daraxen4@gmail.com>
2025-11-27 16:04:07 -0500

    Add function to calculate sum of two integers
    Implement function to calculate sum of two integers, returning triple if equal.

--- Día 14/Task/1-ejercicio.js
+++ Día 14/Task/1-ejercicio.js
@@ -1,1 +1,12 @@
+// Escriba un programa en JavaScript para calcular la suma de los dos numeros enteros dados
+// Si los dos valores son iguales, entonces devuelve el triple del valor
 
+function suma(valorEntero1,valorEntero2){
+    if(valorEntero1 != valorEntero2){
+        return valorEntero1 + valorEntero2;
+    }else {
+        return (valorEntero1+valorEntero2) * 3;
+    }
+}
+console.log(suma(14,14));
+console.log(suma(20,25));
