4293f9c4b48bc28520ca6c605b0987df754a319f
darleyramirez <daraxen4@gmail.com>
2025-12-03 14:55:57 -0500

    Update areaTriangulo function for Heron's formula
    Refactor area calculation to use Heron's formula.

--- Día 15/task/4-exercise.js
+++ Día 15/task/4-exercise.js
@@ -4,14 +4,11 @@ Escriba un programa en JavaScript para encontrar el área de un triángulo donde
 Este programa JavaScript calcula el área de un triángulo con lados de longitud 5, 6 y 7 usando la fórmula de Heron. 
 Primero calcula el semiperímetro del triángulo y luego lo usa para hallar el área, que se registra en la consola
 */
+
 function areaTriangulo (lado1,lado2,lado3){
       let total = lado1+lado2+lado3;
-      let perimetro = total/2;
-      let a = perimetro-lado1;
-      let b = perimetro-lado2;
-      let c = perimetro-lado3;
-      let total1= a+b+c;
-      return total1;
-    
+      let valor = total/2;
+      let resultado = Math.sqrt (valor*(valor-lado1)*(valor-lado2)*(valor-lado3));
+      return resultado;
 }
 console.log(areaTriangulo(5,6,7,));
